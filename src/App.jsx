9fdc778c50a589c7399729ce9453e4a2d29d1d95
Zhibek <thezhibeks@gmail.com>
2024-10-07 17:35:10 +0500

    feat: added state for our feature projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import ProjectList from "./Components/ProjectList";
-import AddProjectList from "./Components/AddProjectForm";
+import AddProjectForm from "./Components/AddProjectForm";
 import Settings from "./Components/Settings";
 
-function App() {
-  return (
-    <div>
-      <h1>Personal Project Manager</h1>
-      <AddProjectList />
-      <ProjectList />
-      <Settings />
-    </div>
-  );
-}
+const App = () => {
+    const [projects, setProjects] = useState([]);
+
+    const addProject = (newProject) => {
+        setProjects((prevProjects) => [...prevProjects, newProject]);
+    };
+
+    return (
+        <div>
+            <h1>Personal Project Manager</h1>
+            <AddProjectForm onAddProject={addProject}/>
+            <ProjectList projects={projects}/>
+            <Settings/>
+        </div>
+    );
+};
 
 export default App;
