dafd6721e72f55ca9586ff085c878eede3380d9a
Zhibek <thezhibeks@gmail.com>
2024-10-07 20:03:28 +0500

    feat: saving data to LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProjectList from "./Components/ProjectList";
 import AddProjectForm from "./Components/AddProjectForm";
 import Settings from "./Components/Settings";
@@ -6,14 +6,33 @@ import Settings from "./Components/Settings";
 const App = () => {
     const [projectList, setProjectList] = useState([]);
 
+    // Загружаем проекты из LocalStorage при первой загрузке компонента
+    useEffect(() => {
+        const storedProjects = localStorage.getItem('projectList');
+        if (storedProjects) {
+            try {
+                setProjectList(JSON.parse(storedProjects)); // Преобразуем только если данные валидны
+        } catch (error) {
+            console.error('Ошибка при загрузке данных из LocalStorage:', error);
+            }
+        }
+    }, []);
+
+    // Сохраняем проект в LocalStorage при изменении projectList
+    useEffect(() => {
+        console.log('Сохроняем в ЛокалСторадж: ', projectList);
+        if (projectList.length > 0) {
+            localStorage.setItem('projectList', JSON.stringify(projectList));
+        }
+    }, [projectList]);
+
     const addProject = (newProject) => {
         setProjectList((prevProjectList) => [...prevProjectList, newProject]);
     };
 
-    // Функция для удаления проекта по его id
     const deleteProject = (projectId) => {
-        setProjectList((prevProjectList) => 
-            prevProjectList.filter(project => project.id !== projectId)
+        setProjectList((prevProjectList) =>
+            prevProjectList.filter((project) => project.id !== projectId)
         );
     };
 
